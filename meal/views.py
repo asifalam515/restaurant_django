8207a27f5c2689609fc4ad882a6493f1f2f02b9e
asifalam515 <asibulalam515@gmail.com>
2023-12-11 12:34:38 +0600

    truncate word filter done

--- meal/views.py
+++ meal/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
-meal={
-  "categories": [
-    {
+
+# Create your views here.
+def home(request):
+    return render(request,'meal/home.html')
+
+def showItem(request):
+    meals=[{
       "idCategory": "1",
       "strCategory": "Beef",
       "strCategoryThumb": "https://www.themealdb.com/images/category/beef.png",
@@ -36,12 +40,5 @@ meal={
       "strCategory": "Pasta",
       "strCategoryThumb": "https://www.themealdb.com/images/category/pasta.png",
       "strCategoryDescription": "Pasta is a staple food of traditional Italian cuisine, with the first reference dating to 1154 in Sicily.\r\n\r\nAlso commonly used to refer to the variety of pasta dishes, pasta is typically a noodle made from an unleavened dough of a durum wheat flour mixed with water or eggs and formed into sheets or various shapes, then cooked by boiling or baking. As an alternative for those wanting a different taste, or who need to avoid products containing gluten, some pastas can be made using rice flour in place of wheat.[3][4] Pastas may be divided into two broad categories, dried (pasta secca) and fresh (pasta fresca)."
-    },
-     ]
-}
-# Create your views here.
-def home(request):
-    return render(request,'meal/home.html')
-
-def showItem(request):
-    return render(request,'meal/items.html',meal)
+    }]
+    return render(request,'meal/items.html',{'meals':meals})
